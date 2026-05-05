a826b065b89cfcf485a61220f24c154135323778
binsky <timo@binsky.org>
2026-05-05 19:27:31 +0200

    add nc32 php8.4 build files; refactor Dockerfile to fit new externally shipped apps requirement and to improve updateability

---
+++ nc32_php8.4/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp ../entrypoint.sh .
+docker build -t binsky/passman-dev:nc32_php8.4 -t binsky/passman-dev:latest .
+#docker push binsky/passman-dev:nc32_php8.4
+#docker push binsky/passman-dev:latest
+rm entrypoint.sh
